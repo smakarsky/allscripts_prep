ef87afc838c1f3c3b47f175b28ba28375471b263
Stan Makarsky <smakarsky@gmail.com>
2011-09-12 12:43:30 -0500

    Pushing new changes

--- source/nothinbutdotnetprep/collections/Movie.cs
+++ source/nothinbutdotnetprep/collections/Movie.cs
@@ -2,12 +2,20 @@ using System;
 
 namespace nothinbutdotnetprep.collections
 {
-  public class Movie
+  public class Movie //: IComparable<Movie>
   {
     public string title { get; set; }
     public ProductionStudio production_studio { get; set; }
     public Genre genre { get; set; }
     public int rating { get; set; }
     public DateTime date_published { get; set; }
+   
+      //public int CompareTo(Movie other)
+      //{
+      //    return
+      //        (other.genre.ToString() + other.date_published.ToString() + other.production_studio + other.rating +
+      //         other.title).CompareTo(this.genre.ToString() + this.date_published.ToString() + this.production_studio +
+      //                                this.rating + this.title);
+      //}
   }
 }
--- source/nothinbutdotnetprep/collections/MovieLibrary.cs
+++ source/nothinbutdotnetprep/collections/MovieLibrary.cs
@@ -14,12 +14,27 @@ namespace nothinbutdotnetprep.collections
 
         public IEnumerable<Movie> all_movies()
         {
-            return this.movies;
+            foreach (var movie in movies)
+            {
+                yield return movie;
+            }
+            //return this.movies;
         }
 
         public void add(Movie movie)
         {
-            throw new NotImplementedException();
+            if (!movies.Contains(movie))
+            {
+                if (movie.title != null)
+                {
+                    foreach (var temp_movie in movies)
+                        if (movie.title == temp_movie.title)
+                            return;
+                }
+
+                this.movies.Add(movie);
+
+            }
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_descending()
@@ -29,12 +44,20 @@ namespace nothinbutdotnetprep.collections
 
         public IEnumerable<Movie> all_movies_published_by_pixar()
         {
-            throw new NotImplementedException();
+            foreach ( var movie in movies)
+            {
+                if (movie.production_studio == ProductionStudio.Pixar)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
         {
-            throw new NotImplementedException();
+            foreach (var movie in movies)
+            {
+                if (movie.production_studio == ProductionStudio.Pixar || movie.production_studio == ProductionStudio.Disney)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_ascending()
@@ -49,27 +72,47 @@ namespace nothinbutdotnetprep.collections
 
         public IEnumerable<Movie> all_movies_not_published_by_pixar()
         {
-            throw new NotImplementedException();
+            foreach (var movie in movies)
+            {
+                if (movie.production_studio != ProductionStudio.Pixar)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_movies_published_after(int year)
         {
-            throw new NotImplementedException();
+            foreach (var movie in movies)
+            {
+                if (movie.date_published.Year > year)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
         {
-            throw new NotImplementedException();
+            foreach (var movie in movies)
+            {
+                if (movie.date_published.Year >= startingYear && movie.date_published.Year <= endingYear)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_kid_movies()
         {
-            throw new NotImplementedException();
+            foreach (var movie in movies)
+            {
+                if (movie.genre == Genre.kids)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_action_movies()
         {
-            throw new NotImplementedException();
+            foreach (var movie in movies)
+            {
+                if (movie.genre == Genre.action)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
@@ -79,6 +122,7 @@ namespace nothinbutdotnetprep.collections
 
         public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
         {
+            //return ((List<Movie>) movies).Sort();
             throw new NotImplementedException();
         }
     }
